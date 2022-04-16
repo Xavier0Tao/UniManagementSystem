07d6a15753f8bd3eb5725c74a223670cbbb57729
Xavier <1059044790@qq.com>
2022-04-16 20:30:21 +0800

    Project StarUP

---
+++ src/main/java/org/uni/UniManaSysApplication.java
@@ -0,0 +1,13 @@
+package org.uni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniManaSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniManaSysApplication.class, args);
+    }
+
+}
---
+++ src/main/resources/sql/console.sql
@@ -0,0 +1,5 @@
+use mysql;
+
+# 创建数据库
+CREATE DATABASE wangt_mis;
+
